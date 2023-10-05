e776b42357c64aa5927b16dce673f95881eeb1bc
AnastasiiaDia <anastasiia.iv@gmail.com>
2023-10-05 21:45:04 +0200

    added hooks

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,13 +2,13 @@ import { FeedBackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Notification } from 'components/Notification/Notification';
 import { Section } from 'components/Section/Section';
 import { Statistics } from 'components/Statistics/Statistics';
-import React, { Component, useState } from 'react';
+import { useState } from 'react';
 import { Container } from './Feedback.styled';
 export default function Feedback() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const feedback = { good, neutral, bad };
+  const options = { good, neutral, bad };
 
   const onTotalFeedback = () => {
     return good + neutral + bad;
@@ -36,14 +36,14 @@ export default function Feedback() {
   return (
     <Container>
       <Section title={`Please leave feedback`}>
-        <FeedBackOptions options={feedback} onLeaveFeedback={onLeaveFeedback} />
+        <FeedBackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
       </Section>
       {totalFeedback === 0 ? (
         <Notification message={`No feedback given`} />
       ) : (
         <Section title={`Statistics`}>
           <Statistics
-            options={feedback}
+            options={options}
             totalFeedback={onTotalFeedback()}
             positivePercentage={positivePercentage()}
           />
